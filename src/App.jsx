861bdd53f671d82d6644f77c6f8bbdb3fc01acf0
devleet4305 <tahajibmunna@gmail.com>
2026-05-08 01:42:37 +0600

    added all project files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <h1>vite+ react</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+    </>
+  )
+}
+function Person() {
+  return (
+   <p>i am a person</p>
+  )
+}
+export default App
